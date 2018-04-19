ed03e629e076108248ce8d96e16ca214bca86774
Anton Furuholm <anton.furuholm@gmail.com>
2018-04-19 09:54:45 +0200

    Add dev server

--- docs/dist/script.es5.js
+++ docs/dist/script.es5.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /**
  * AJAX call
  * Works as the normal fetch(),
--- rollup.config.js
+++ rollup.config.js
@@ -2,36 +2,54 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 import packageJson from './package.json';
+import serve from 'rollup-plugin-serve';
 
 const name = packageJson.name;
 
-const config = {
-    input: name + '.js',
-    output: {
-        file: name + '.es5.js', // equivalent to --output
-        format: 'iife',
-        name: 'ajax'
-    },
-	plugins: [
-        resolve(),
-		babel({
-			exclude: 'node_modules/**', // only transpile our source code
-		}),
-		uglify(),
-	],
+let config;
+
+if (process.env.SERVE == 'dev') {
+    config = {
+        input: 'docs/script.js',
+        output: {
+            file: 'docs/dist/script.es5.js',
+            format: 'es',
+        },
+        plugins: [
+            serve('docs'),
+        ]
+    }
+} else {
+    config = {
+        input: name + '.js',
+        output: {
+            file: name + '.es5.js', // equivalent to --output
+            format: 'iife',
+            name: 'ajax'
+        },
+        plugins: [
+            resolve(),
+            babel({
+                exclude: 'node_modules/**', // only transpile our source code
+            }),
+            uglify(),
+        ],
+    }
+    
+    if (process.env.BUILD == 'es') {
+        config.output = {
+            file: name + '.min.js', // equivalent to --output
+            format: 'es',
+        };
+    } else if (process.env.BUILD == 'cjs') {
+        config.output = {
+            file: name + '.cjs.js', // equivalent to --output
+            format: 'cjs',
+            // exports: 'named',
+        };
+    }
 }
 
-if (process.env.BUILD == 'es') {
-    config.output = {
-        file: name + '.min.js', // equivalent to --output
-        format: 'es',
-    };
-} else if (process.env.BUILD == 'cjs') {
-    config.output = {
-        file: name + '.cjs.js', // equivalent to --output
-        format: 'cjs',
-        // exports: 'named',
-    };
-}
+
 
 export default config;
